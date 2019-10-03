bcceb9ff47f327b2263685b4d6bfc71f833bef94
Joe Downs <joedowns502@gmail.com>
2019-10-03 11:19:11 -0400

    1.3.4-product-calc: alternate solution to 1.3.4 activity

---
+++ zyBooks/1.3.4-product-calc.py
@@ -0,0 +1,16 @@
+answer = " "
+numbers_array = []
+
+while answer != "":
+    answer = input("Choose your numbers: ")
+    if answer != "":
+        numbers_array.append(int(answer))
+        #print(numbers_array)
+        
+product = 1
+for entry in numbers_array:
+    #print(entry)
+    product *= entry
+
+print("The product is: " + str(product))
+                            
