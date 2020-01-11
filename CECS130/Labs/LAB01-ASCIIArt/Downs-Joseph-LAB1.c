ccfb62d7f0d4085b24b945fac8a81c7123f5435f
Joe Downs <joedowns502@gmail.com>
2020-01-11 00:28:00 -0500

    LAB1: add program description comments; add empty print statements to populate with lines from planned-output.txt

---
+++ CECS130/Labs/LAB01-ASCIIArt/Downs-Joseph-LAB1.c
@@ -0,0 +1,47 @@
+/*
+Name: Joseph Downs
+Lab #1
+Date: 2020.01.09
+Description: Prints our ASCII art picture of Dr. Yampolskiy
+ */
+#include <stdio.h>
+
+//Prints out ASCII art picture
+int main()
+{
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+  printf(" \n");
+}
