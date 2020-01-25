3718eaacd8a9b575da6c2cca59e6a8cc62bccf14
Joe Downs <joedowns502@gmail.com>
2020-01-25 15:50:32 -0500

    LAB03.c: initial commit with description, and standard code beginnings

---
+++ CECS130/Labs/LAB03-Calculator/Downs-Joseph-LAB03.c
@@ -0,0 +1,17 @@
+/*
+Name: Joseph Downs
+Lab 02
+2020.01.24
+
+A simple calculator that can perform addition, subtraction, multiplication,
+division, modulus, and test if integers are prime.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  
+
+  
+}
