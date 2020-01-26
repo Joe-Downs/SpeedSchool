420fb574992d83a7dba225dba34b1405f899a932
Joe Downs <joedowns502@gmail.com>
2020-01-25 23:30:45 -0500

    LAB03.c: change description

--- CECS130/Labs/LAB03-Calculator/Downs-Joseph-LAB03.c
+++ CECS130/Labs/LAB03-Calculator/Downs-Joseph-LAB03.c
@@ -4,7 +4,7 @@ Lab 02
 2020.01.24
 
 A simple calculator that can perform addition, subtraction, multiplication,
-division, modulus, and test if integers are prime.
+division, modulus, and a primality test.
 */
 
 #include <stdio.h>
