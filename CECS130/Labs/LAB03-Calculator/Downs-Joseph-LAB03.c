54fdab5086e30b5223055151c1c35c9edaeed07b
Joe Downs <jadown07@louisville.edu>
2020-01-27 17:18:15 -0500

    LAB03.c: change header comment style

--- CECS130/Labs/LAB03-Calculator/Downs-Joseph-LAB03.c
+++ CECS130/Labs/LAB03-Calculator/Downs-Joseph-LAB03.c
@@ -1,11 +1,11 @@
 /*
-Name: Joseph Downs
-Lab 02
-2020.01.24
-
-A simple calculator that can perform addition, subtraction, multiplication,
-division, modulus, and a primality test.
-*/
+ * Name: Joseph Downs
+ * Lab 02
+ * 2020.01.24
+ *
+ * A simple calculator that can perform addition, subtraction, multiplication,
+ * division, modulus, and a primality test.
+ */
 
 #include <stdio.h>
 
