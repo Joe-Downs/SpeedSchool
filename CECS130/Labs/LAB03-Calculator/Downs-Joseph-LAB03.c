1a2095e0e9036178beb9491e56cf8e06ab73bc87
Joe Downs <jadown07@louisville.edu>
2020-01-29 14:39:44 -0500

    LAB03: add operation_header() function to dispay operation name and take input for 5 of the six operations

--- CECS130/Labs/LAB03-Calculator/Downs-Joseph-LAB03.c
+++ CECS130/Labs/LAB03-Calculator/Downs-Joseph-LAB03.c
@@ -10,6 +10,13 @@
 #include <stdio.h>
 
 char menu_prompt();
+void operation_header(char operation);
+float addition(float num1, float num2);
+float subtraction(float num1, float num2);
+float multiplication(float num1, float num2);
+float division(float num1, float num2);
+float modulus(float num1, float num2);
+void primality_test(float num1);
 
 int main()
 {
@@ -34,4 +41,26 @@ int main()
     return user_input;
   }
   char user_choice = menu_prompt();
+  float num1;
+  float num2;
+  char operation[];
+  /* 
+   * operation_header() uses a pre-defined string variable, and prints out a
+   * header declaring the operation, and prompts the user for two numbers.
+   * Applicable for addition, subtraction, division, multiplication, and modulus.
+   */
+  void operation_header()
+  {
+    printf("\n");
+    printf("=====%s=====\n", operation);
+    printf("Enter your first number: ");
+    scanf("%f", &num1);
+    printf("Enter your second number: ");
+    scanf("%f", &num2);
+  }
+  
+  float addition(float num1, float num2)
+  {
+    
+  }
 }
