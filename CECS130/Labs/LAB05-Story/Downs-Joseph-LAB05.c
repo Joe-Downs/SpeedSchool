66b15fa26846b79cf4db8d9157addd8645d5447f
Joe Downs <joedowns502@gmail.com>
2020-02-11 01:53:51 -0500

    LAB05.c: initial commit with program header and main() declaration

---
+++ CECS130/Labs/LAB05-Story/Downs-Joseph-LAB05.c
@@ -0,0 +1,17 @@
+/*
+ * Joseph Downs
+ * LAB 05
+ * 2020.02.15
+ * This program generates interactive stories by asking the user information
+ * about themselves, and inputting that information into a defined structure.
+ */
+
+#include <stdio.h>
+
+
+int main()
+{
+  
+
+  
+}
