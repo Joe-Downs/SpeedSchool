011fed742c3308a542da8e6d0a9af744e579f46e
Joe Downs <jadown07@louisville.edu>
2020-02-24 13:23:44 -0500

    LAB06.c: add add_-, delete_-, and list_contact functions

--- CECS130/Labs/LAB06-Phonebook/Downs-Joseph-LAB06.c
+++ CECS130/Labs/LAB06-Phonebook/Downs-Joseph-LAB06.c
@@ -9,19 +9,34 @@
 #include <stdio.h>
 
 void menu_prompt();
+void add_contact();
+void delete_contact();
+void list_contacts();
 
 int user_choice = 0;
 
 void menu_prompt()
 {
   printf("Phone Book\n");
-  printf(" 1. Add Friend\n");
-  printf(" 2. Delete Friend\n");
-  printf(" 3. List contacts\n");
+  printf(" 1. Add Contact\n");
+  printf(" 2. Delete Contact\n");
+  printf(" 3. List Contacts\n");
   printf(" 4. Exit\n");
   printf("\n");
   printf("Choose an option: ");
   scanf("%d", &user_choice);
+}
+
+void add_contact()
+{
+}
+
+void delete_contact()
+{
+}
+
+void list_contact()
+{
 }
 
 int main()
@@ -29,5 +44,17 @@ int main()
   while (user_choice != 4)
     {
       menu_prompt();
+      if (user_choice == 1)
+	{
+	  add_contact();
+	}
+      else if (user_choice == 2)
+	{
+	  remove_contact();
+	}
+      else if (user_choice == 3)
+	{
+	  list_contacts()
+	}
     }
 }
