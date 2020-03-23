da55864e16869059fc58f1f6f665410fa655d37e
Joe Downs <joedowns502@gmail.com>
2020-03-22 21:48:22 -0400

    LAB09.cpp: add lab header, include statements, and main()

---
+++ CECS130/Labs/LAB09-Tic-Tac-Toe/Downs-Joseph-LAB09.cpp
@@ -0,0 +1,18 @@
+/*
+ * Joseph Downs
+ * LAB 09
+ * 2020.03.27
+ * This programs utilzies ASCII art to display a Tic-Tac-Toe
+ * board. The user plays against the computer (who goes first
+ * is randomly decided before play begins). O's and X's are 
+ * 'drawn' in after each move is made. If an illegal move is
+ * made, the user is informed so that they can try again.
+ */
+
+#include <iostream>
+#include <ctype.h>
+
+int main()
+{
+  
+}
