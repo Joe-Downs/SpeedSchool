de2fdf2c4035a22a01329ce42bac1abd61a6f72a
Joe Downs <joedowns502@gmail.com>
2020-03-28 03:16:15 -0400

    LAB09.cpp: add update_board() function
    ~ Takes location and choice as arguments and updates the board
      to correctly display what the user / computer player has
      chosen to play and where.
    + Added row and column headers to output of print_board() function

--- CECS130/Labs/LAB09-Tic-Tac-Toe/Downs-Joseph-LAB09.cpp
+++ CECS130/Labs/LAB09-Tic-Tac-Toe/Downs-Joseph-LAB09.cpp
@@ -15,10 +15,12 @@
 
 using namespace std;
 
+void update_board(char row, int column, char choice);
+void print_board();
+
 class board
 {
 public:
-  void print_board();
   /* Locations for each square; can be empty, X, or O */
   char A1, A2, A3;
   char B1, B2, B3;
@@ -26,6 +28,7 @@ public:
 };
 
 board TTT_board;
+
 
 /* 
  * Arays to hold the strings used to construct the TTT board.
@@ -40,27 +43,114 @@ string B3[3] = {"     ", "     ", "     "};
 string C1[3] = {"     ", "     ", "     "};
 string C2[3] = {"     ", "     ", "     "};
 string C3[3] = {"     ", "     ", "     "};
+
+void update_board(char row, int column, char choice)
+{
+  string line1;
+  string line2;
+  string line3;
+  if (choice == 'X')
+    {
+      line1 = " \\ / ";
+      line2 = "  X  ";
+      line3 = " / \\ ";
+    }
+  else
+    {
+      line1 = " / \\ ";
+      line2 = " | | ";
+      line3 = " \\ / ";
+    }
+  if (row == 'A')
+    {
+      if (column == 1)
+	{
+	  A1[0] = line1;
+	  A1[1] = line2;
+	  A1[2] = line3;
+	}
+      else if (column == 2)
+	{
+	  A2[0] = line1;
+	  A2[1] = line2;
+	  A2[2] = line3;
+	}
+      else if (column == 3)
+	{
+	  A3[0] = line1;
+	  A3[1] = line2;
+	  A3[2] = line3;
+	}
+    }
+  else if (row == 'B')
+    {   
+      if (column == 1)
+	{
+	  B1[0] = line1;
+	  B1[1] = line2;
+	  B1[2] = line3;
+	}
+      else if (column == 2)
+	{
+	  B2[0] = line1;
+	  B2[1] = line2;
+	  B2[2] = line3;
+	}
+      else if (column == 3)
+	{
+	  B3[0] = line1;
+	  B3[1] = line2;
+	  B3[2] = line3;
+	}
+    }
+  else if (row == 'C')
+    {   
+      if (column == 1)
+	{
+	  C1[0] = line1;
+	  C1[1] = line2;
+	  C1[2] = line3;
+	}
+      else if (column == 2)
+	{
+	  C2[0] = line1;
+	  C2[1] = line2;
+	  C2[2] = line3;
+	}
+      else if (column == 3)
+	{
+	  C3[0] = line1;
+	  C3[1] = line2;
+	  C3[2] = line3;
+	}
+    }
+}
   
 /* Prints out updated board after each move */
 void print_board()
 {
   cout<<endl;
-  cout<<A1[0]<<"|"<<A2[0]<<"|"<<A3[0]<<endl;
-  cout<<A1[1]<<"|"<<A2[1]<<"|"<<A3[1]<<endl;
-  cout<<A1[2]<<"|"<<A2[2]<<"|"<<A3[2]<<endl;
-  cout<<"-----+-----+-----"<<endl;
-  cout<<B1[0]<<"|"<<B2[0]<<"|"<<B3[0]<<endl;
-  cout<<B1[1]<<"|"<<B2[1]<<"|"<<B3[1]<<endl;
-  cout<<B1[2]<<"|"<<B2[2]<<"|"<<B3[2]<<endl;
-  cout<<"-----+-----+-----"<<endl;
-  cout<<C1[0]<<"|"<<C2[0]<<"|"<<C3[0]<<endl;
-  cout<<C1[1]<<"|"<<C2[1]<<"|"<<C3[1]<<endl;
-  cout<<C1[2]<<"|"<<C2[2]<<"|"<<C3[2]<<endl;
+  cout<<"    1     2     3         "<<endl; 
+  cout<<"  "<<A1[0]<<"|"<<A2[0]<<"|"<<A3[0]<<endl;
+  cout<<"A "<<A1[1]<<"|"<<A2[1]<<"|"<<A3[1]<<endl;
+  cout<<"  "<<A1[2]<<"|"<<A2[2]<<"|"<<A3[2]<<endl;
+  cout<<"  -----+-----+-----"<<endl;
+  cout<<"  "<<B1[0]<<"|"<<B2[0]<<"|"<<B3[0]<<endl;
+  cout<<"B "<<B1[1]<<"|"<<B2[1]<<"|"<<B3[1]<<endl;
+  cout<<"  "<<B1[2]<<"|"<<B2[2]<<"|"<<B3[2]<<endl;
+  cout<<"  -----+-----+-----"<<endl;
+  cout<<"  "<<C1[0]<<"|"<<C2[0]<<"|"<<C3[0]<<endl;
+  cout<<"C "<<C1[1]<<"|"<<C2[1]<<"|"<<C3[1]<<endl;
+  cout<<"  "<<C1[2]<<"|"<<C2[2]<<"|"<<C3[2]<<endl;
   cout<<endl;
 }
 
 
 int main()
 {
+  print_board();
+  update_board('A', 1, 'O');
+  print_board();
+  update_board('C', 3, 'X');
   print_board();
 }
