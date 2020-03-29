fdbd5a4c643ae4ebc0fc176abaff8a993a3a3b0f
Joe Downs <joedowns502@gmail.com>
2020-03-29 04:21:56 -0400

    LAB09.cpp: add one array to handle the board tiles
      This simplified the mess of conditionals in the update_board()
      function and will simplify others as well.
    
    ~ Changed print_board() function to work with new single array

--- CECS130/Labs/LAB09-Tic-Tac-Toe/Downs-Joseph-LAB09.cpp
+++ CECS130/Labs/LAB09-Tic-Tac-Toe/Downs-Joseph-LAB09.cpp
@@ -35,19 +35,16 @@ public:
 board TTT_board;
 
 
-/* 
- * Arays to hold the strings used to construct the TTT board.
- * Each index is a line that makes up the three lines of the block
- */
-string A1[3] = {"     ", "     ", "     "};
-string A2[3] = {"     ", "     ", "     "};
-string A3[3] = {"     ", "     ", "     "};
-string B1[3] = {"     ", "     ", "     "};
-string B2[3] = {"     ", "     ", "     "};
-string B3[3] = {"     ", "     ", "     "};
-string C1[3] = {"     ", "     ", "     "};
-string C2[3] = {"     ", "     ", "     "};
-string C3[3] = {"     ", "     ", "     "};
+/* Arrays to hold the strings used to construct the TTT board. */
+string squares[27] = {"     ", "     ", "     ",  /* A1 */
+		      "     ", "     ", "     ",  /* A2 */
+		      "     ", "     ", "     ",  /* A3 */
+		      "     ", "     ", "     ",  /* B1 */
+		      "     ", "     ", "     ",  /* B2 */
+		      "     ", "     ", "     ",  /* B3 */
+		      "     ", "     ", "     ",  /* C1 */
+		      "     ", "     ", "     ",  /* C2 */
+		      "     ", "     ", "     ",};/* C3 */
 
 void update_board(char row, int column, char choice)
 {
@@ -66,87 +63,56 @@ void update_board(char row, int column, char choice)
       line2 = " | | ";
       line3 = " \\ / ";
     }
+  int row_offset;
   if (row == 'A')
     {
-      if (column == 1)
-	{
-	  A1[0] = line1;
-	  A1[1] = line2;
-	  A1[2] = line3;
-	}
-      else if (column == 2)
-	{
-	  A2[0] = line1;
-	  A2[1] = line2;
-	  A2[2] = line3;
-	}
-      else if (column == 3)
-	{
-	  A3[0] = line1;
-	  A3[1] = line2;
-	  A3[2] = line3;
-	}
+      row_offset = 0;
     }
   else if (row == 'B')
-    {   
-      if (column == 1)
-	{
-	  B1[0] = line1;
-	  B1[1] = line2;
-	  B1[2] = line3;
-	}
-      else if (column == 2)
-	{
-	  B2[0] = line1;
-	  B2[1] = line2;
-	  B2[2] = line3;
-	}
-      else if (column == 3)
-	{
-	  B3[0] = line1;
-	  B3[1] = line2;
-	  B3[2] = line3;
-	}
+    {
+      row_offset = 9;
     }
   else if (row == 'C')
-    {   
-      if (column == 1)
-	{
-	  C1[0] = line1;
-	  C1[1] = line2;
-	  C1[2] = line3;
-	}
-      else if (column == 2)
-	{
-	  C2[0] = line1;
-	  C2[1] = line2;
-	  C2[2] = line3;
-	}
-      else if (column == 3)
-	{
-	  C3[0] = line1;
-	  C3[1] = line2;
-	  C3[2] = line3;
-	}
+    {
+      row_offset = 18;
     }
+  int index = 3 * (column - 1) + row_offset;
+  squares[index] = line1;
+  squares[index+1] = line2;
+  squares[index+2] = line3;
 }
   
 /* Prints out updated board after each move */
 void print_board()
 {
+  int n;
+  string row_letter;
+  string row_blank = "  ";
+  string * letter_pointer = &row_letter;
+  string * blank_pointer = &row_blank;
+  string * row_label[3] = {blank_pointer, letter_pointer, blank_pointer};
   cout<<endl;
-  cout<<"    1     2     3         "<<endl; 
-  cout<<"  "<<A1[0]<<"|"<<A2[0]<<"|"<<A3[0]<<endl;
-  cout<<"A "<<A1[1]<<"|"<<A2[1]<<"|"<<A3[1]<<endl;
-  cout<<"  "<<A1[2]<<"|"<<A2[2]<<"|"<<A3[2]<<endl;
+  cout<<"    1     2     3         "<<endl;
+  row_letter = "A ";
+  for (n = 0; n < 3; n++)
+    {
+      cout<<*row_label[n];
+      cout<<squares[n]<<"|"<<squares[n+3]<<"|"<<squares[n+6]<<endl;
+    }
   cout<<"  -----+-----+-----"<<endl;
-  cout<<"  "<<B1[0]<<"|"<<B2[0]<<"|"<<B3[0]<<endl;
-  cout<<"B "<<B1[1]<<"|"<<B2[1]<<"|"<<B3[1]<<endl;
-  cout<<"  "<<B1[2]<<"|"<<B2[2]<<"|"<<B3[2]<<endl;
+  row_letter = "B ";
+  for (n = 0; n < 3; n++)
+    {
+      cout<<*row_label[n];
+      cout<<squares[n+9]<<"|"<<squares[n+12]<<"|"<<squares[n+15]<<endl;
+    }
   cout<<"  -----+-----+-----"<<endl;
-  cout<<"  "<<C1[0]<<"|"<<C2[0]<<"|"<<C3[0]<<endl;
-  cout<<"C "<<C1[1]<<"|"<<C2[1]<<"|"<<C3[1]<<endl;
-  cout<<"  "<<C1[2]<<"|"<<C2[2]<<"|"<<C3[2]<<endl;
+  row_letter = "C ";
+  for (n = 0; n < 3; n++)
+    {
+      cout<<*row_label[n];
+      cout<<squares[n+18]<<"|"<<squares[n+21]<<"|"<<squares[n+24]<<endl;
+    }
   cout<<endl;
 }
 
