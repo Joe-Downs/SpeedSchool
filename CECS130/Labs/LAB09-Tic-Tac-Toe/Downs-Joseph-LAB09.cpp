92c8ff943611f0e22b2aa513632d9fec81ec180b
Joe Downs <joedowns502@gmail.com>
2020-03-30 02:08:24 -0400

    LAB09.cpp: change a lot

--- CECS130/Labs/LAB09-Tic-Tac-Toe/Downs-Joseph-LAB09.cpp
+++ CECS130/Labs/LAB09-Tic-Tac-Toe/Downs-Joseph-LAB09.cpp
@@ -18,21 +18,31 @@ using namespace std;
 
 void update_board(char row, int column, char choice);
 void print_board();
+int board_plays_index(char row, int column);
 void user_move();
 void cpu_move();
 bool is_valid(char row, int column);
+bool do_match(char row1, int col1, char row2, int col2);
 bool is_over();
 
-class board
+class Board
 {
 public:
-  /* Locations for each square; can be empty, X, or O */
-  char A1, A2, A3;
-  char B1, B2, B3;
-  char C1, C2, C3;
+  void place_piece(char row, int column, char choice);
+  /* 
+   * Locations for each square; can be empty, X, or O
+   * Index 0 = A1, 1 = A2, 2 = A3, and so on
+   */
+  char board_plays[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 };
 
-board TTT_board;
+Board TTT_board;
+
+void Board::place_piece(char row, int column, char choice)
+{
+  int play_index = board_plays_index(row, column);
+  board_plays[play_index] = choice;
+}
 
 
 /* Arrays to hold the strings used to construct the TTT board. */
@@ -115,102 +125,59 @@ void print_board()
     }
   cout<<endl;
 }
-
-bool is_valid(char row, int column)
+int board_plays_index(char row, int column)
 {
+  int row_offset;
+  int index;
+  if (column > 3)
+    {
+      return -1;
+    }
   if (row == 'A')
     {
-      if (column == 1)
-	{
-	  if (TTT_board.A1 == 'X'|| TTT_board.A1 == 'O')
-	    {
-	      return false;
-	    }
-	  return true;
-	}
-      else if (column == 2)
-	{
-	  if (TTT_board.A2 == 'X'|| TTT_board.A2 == 'O')
-	    {
-	      return false;
-	    }
-	  return true; 
-	}
-      else if (column == 3)
-	{
-	  if (TTT_board.A3 == 'X'|| TTT_board.A3 == 'O')
-	    {
-	      return false;
-	    }
-	   return true;
-	}
+      row_offset = 0;
     }
   else if (row == 'B')
-    {   
-      if (column == 1)
-	{
-	  if (TTT_board.B1 == 'X'|| TTT_board.B1 == 'O')
-	    {
-	      return false;
-	    }
-	  return true;
-	}
-      else if (column == 2)
-	{
-	  if (TTT_board.B2 == 'X'|| TTT_board.B2 == 'O')
-	    {
-	      return false;
-	    }
-	  return true;
-	}
-      else if (column == 3)
-	{
-	  if (TTT_board.B3 == 'X'|| TTT_board.B3 == 'O')
-	    {
-	      return false;
-	    }
-	  return true;
-	}
+    {
+      row_offset = 3;
     }
   else if (row == 'C')
-    {   
-      if (column == 1)
-	{
-	  if (TTT_board.C1 == 'X'|| TTT_board.C1 == 'O')
-	    {
-	      return false;
-	    }
-	  return true;
-	}
-      else if (column == 2)
-	{
-	  if (TTT_board.C2 == 'X'|| TTT_board.C2 == 'O')
-	    {
-	      return false;
-	    }
-	  return true;
-	}
-      else if (column == 3)
-	{
-	  if (TTT_board.C3 == 'X'|| TTT_board.C3 == 'O')
-	    {
-	      return false;
-	    }
-	  return true;
-	}
+    {
+      row_offset = 6;
     }
-  return false;
+  index = row_offset + (column - 1);
+  return index;
+}
+
+
+bool is_valid(int index)
+{
+  if (index == -1)
+    {
+      return false;
+    }
+  if (TTT_board.board_plays[index] == 'X' || TTT_board.board_plays[index] == 'O')
+    {
+      return false;
+    }
+  else
+    {
+      return true;
+    }
 }
 
 void user_move()
 {
   char user_row;
   int user_column;
+  int index;
   cout<<"Choose a square: ";
   cin>>user_row>>user_column;
-  if (is_valid(user_row, user_column))
+  index = board_plays_index(user_row, user_column);
+  if (is_valid(index))
     {
       update_board(user_row, user_column, 'X');
+      TTT_board.place_piece(user_row, user_column, 'X');
       return;
     }
   else
@@ -225,9 +192,77 @@ void cpu_move()
   
 }
 
+/* Checks if two spaces have the same choice */
+bool do_match(char row1, int col1, char row2, int col2)
+{
+  int index1 = board_plays_index(row1, col1);
+  int index2 = board_plays_index(row2, col2);
+  /*
+  cout<<TTT_board.board_plays[index1]<<endl;
+  cout<<TTT_board.board_plays[index2]<<endl;
+  */
+  if (TTT_board.board_plays[index1] == ' ' || TTT_board.board_plays[index2] == ' ')
+    {
+      return false;
+    }
+  if (TTT_board.board_plays[index1] == TTT_board.board_plays[index2])
+    {
+      return true;
+    }
+  else
+    {
+      return false;
+    }
+}
+
 bool is_over()
 {
-  
+  int n, i;
+  int row_matches;
+  int col_matches;
+  char rows[3] = {'A', 'B', 'C'};
+  for (n = 0; n < 3; n++)
+    {
+      row_matches = 0;
+      col_matches = 0;
+      for (i = 0; i < 2; i++)
+	{
+	  if (do_match(rows[n], i+1, rows[n], i+2))
+	    {
+	      cout<<"1"<<endl;
+	      row_matches += 1;
+	    }
+	  cout<<rows[i]<<n+1<<endl;
+	  cout<<rows[i+1]<<n+1<<endl;
+	  if (do_match(rows[i], n+1, rows[i+1], n+1))
+	    {
+	      cout<<"2"<<endl;
+	      col_matches += 1;
+	    }
+	}
+      if (row_matches == 2)
+	{
+	  cout<<"3"<<endl;
+	  return true;
+	}
+      if (col_matches == 2)
+	{
+	  cout<<"4"<<endl;
+	  return true;
+	}
+    }
+  if (do_match(rows[0], 1, rows[1], 2) && do_match(rows[1], 2, rows[2], 3))
+    {
+      cout<<"5"<<endl;
+      return true;
+    }
+  if (do_match(rows[2], 1, rows[1], 2) && do_match(rows[1], 2, rows[0], 3))
+    {
+      cout<<"6"<<endl;
+      return true;
+    }
+  cout<<"7"<<endl;
+  return false;
 }
 
 
@@ -285,6 +320,7 @@ int main()
 	}
       while (!is_over());
     }
+  print_board();
   cout<<win_message<<endl;
   cout<<endl;
   cout<<"Thanks for playing!"<<endl;
