203447f3575c738c99edbb562fa4f994a99979f7
Joe Downs <joedowns502@gmail.com>
2020-04-24 01:39:33 -0400

    FINAL.cpp: add main game functions
    + playPoker()
    + takeBet()
    + playHand()
    + outcome()

--- CECS130/Downs-Joseph-FINAL.cpp
+++ CECS130/Downs-Joseph-FINAL.cpp
@@ -17,14 +17,17 @@ using namespace std;
 
 void shuffleDeck();
 void playPoker();
+int takeBet();
+void playHand(int bet);
+void outcome(int bet);
+void generateReport();
 
 /* Card class that has a numeric value and a suit */
 class Card
 {
-private:
+public:
   int value;
   char suit;
-public:
   Card(int val, char s)
   {
     value = val;
@@ -49,6 +52,7 @@ Card deck[36] = {{1, 'H'}, {2, 'H'}, {3, 'H'}, {4, 'H'}, {5, 'H'}, {6, 'H'}, {7,
 void shuffleDeck()
 {
   srand(time(NULL));
+  cout<<"Everyday I'm shuffling..."<<endl;
   random_shuffle(begin(deck), end(deck));
 }
 
@@ -59,7 +63,123 @@ int handsWon = 0;
 
 void playPoker()
 {
-  
+  char playerResponse;
+  int playerBet;
+  cout<<"Would you like to play a hand? ";
+  cin>>playerResponse;
+  if (playerResponse == 'n' || playerResponse == 'N')
+    {
+      return;
+    }
+  if (playerBalance <= 0)
+    {
+      cout<<"Too rich for your blood, sorry."<<endl;
+      return;
+    }
+  else
+    {
+      cin.clear();
+      cin.ignore(1000, '\n');
+      playerBet = takeBet();
+      playHand(playerBet);
+      playPoker();
+    }
+}
+
+int takeBet()
+{
+  int bet = 0;
+  cout<<"What is your bet? [1-"<<playerBalance<<"] ";
+  cin>>bet;
+  if (bet >= 1 && bet <= playerBalance)
+    {
+      return bet;
+    }
+  cin.clear();
+  cin.ignore(1000, '\n');
+  takeBet();
+}
+
+/* Takes bet, plays hand, and updates bank accordingly by calling outcome() */
+void playHand(int bet)
+{
+  shuffleDeck();
+  cout<<endl;
+  int i;
+  for (i = 0; i < 4; i++)
+    {
+      deck[i].printCard();
+    }
+  cout<<endl;
+  cout<<endl;
+  outcome(bet);
+}
+
+/* Determines and prints the results and updates the bank */
+void outcome(int bet)
+{
+  handsPlayed += 1;
+  int winMultiplier;
+  string winType;
+  int n;
+  int sortedValues[4];
+  for (n = 0; n < 3; n++)
+    {
+      sortedValues[n] = deck[n].value;
+    }
+  sort(begin(sortedValues), end(sortedValues));
+  int val1 = sortedValues[0];
+  int val2 = sortedValues[1];
+  int val3 = sortedValues[2];
+  int val4 = sortedValues[3];
+  char suit1 = deck[0].suit;
+  char suit2 = deck[1].suit;
+  char suit3 = deck[2].suit;
+  char suit4 = deck[3].suit;
+  if ((val1 == val2) && (val2 == val3) && (val3 == val4))
+    {
+      winMultiplier = 400;
+      winType = "Four of a Kind";
+    }
+  else if ((val1 + 1 == val2) && (val2 + 1 == val3) && (val3 + 1 == val4))
+    {
+      winMultiplier = 200;
+      winType = "Straight";
+      if ((suit1 == suit2) && (suit2 == suit3) && (suit3 == suit4))
+	{
+	  winMultiplier = 300;
+	  winType = "Straight Flush";
+	}
+    }
+  else if ((suit1 == suit2) && (suit2 == suit3) && (suit3 == suit4))
+    {
+      winMultiplier = 250;
+      winType = "Flush";
+    }
+  else if (((val1 == val2) && (val2 == val3)) || ((val2 == val3) && (val3 == val4)))
+    {
+      winMultiplier = 150;
+      winType = "Three of a Kind";
+    }
+  else if (((val1 == val2) && (val3 == val4)) || ((val1 == val4) && (val2 == val3)))
+    {
+      winMultiplier = 100;
+      winType = "Two Pair";
+    }
+  else if ((val1 == val2) || (val1 == val3) || (val1 == val4) || (val2 == val3) || (val2 == val4) || (val3 == val4))
+    {
+      winMultiplier = 1;
+      winType = "Two of a Kind";
+    }
+  else
+    {
+      cout<<"Sorry, you got Bubkiss and have lost "<<bet<<" USD."<<endl;
+      playerBalance -= bet;
+      return;
+    }
+  handsWon += 1;
+  playerBalance += (bet * winMultiplier);
+  cout<<"Congratulations! You got a "<<winType<<" and made "<<bet * winMultiplier<<" USD."<<endl;
 }
 
 void generateReport()
@@ -70,7 +190,7 @@ void generateReport()
 int main()
 {
   cout<<"Welcome to Poker!"<<endl;
-  cout<<"You start with $"<<playerBalance<<endl;
+  cout<<"You start with "<<playerBalance<<endl;
   cout<<endl;
   playPoker();
   generateReport();
