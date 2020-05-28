771d7c63f7f3f51541f370af8a96566c09ed1ce9
Joe Downs <joedowns502@gmail.com>
2020-05-27 21:58:58 -0400

    dateFormatter: add code to take date and reformat it
    + Take input, separating it into the day, month, and year using
      Scanner's custom delimiter
    + Print newly formatted date using separate pieces of the date

--- CECS220/Homework/Assignment1/dateFormatter.java
+++ CECS220/Homework/Assignment1/dateFormatter.java
@@ -4,9 +4,17 @@ public class dateFormatter
 {
     public static void main(String[] args)
     {
-
+	String month, day, year;
+	Scanner scan = new Scanner(System.in);
+	/* useDelimiter("/") sets the delimiter to / to separate the dates */
+	scan.useDelimiter("/|\\s");
 	
-    }
+	System.out.print("Enter a date in the MM/DD/YYYY format: ");
+	/* Scans the users input up to the /, marking each field */
+	month = scan.next();
+	day = scan.next();
+	year = scan.next();
 
-    
+	System.out.println("European date format (DD.MM.YYYY): " + day + "." + month + "." + year);
+    }
 }
